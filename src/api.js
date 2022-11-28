77c1e84e21818b8d96832c20842a0208690c3158
Sachin <prasharsachin1@gmail.com>
2022-11-28 16:21:35 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export const geoApioptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cfa6fa4705mshc531b1ea9487890p17f9ebjsn1088c53be468',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY ="b8f80372d87023e7feafe998daa2499b";
+//api.openweathermap.org/data/2.5/?lat={lat}&lon={lon}&appid={API key}
+
